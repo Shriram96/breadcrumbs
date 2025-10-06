04fc173c99f6ca75926bfeeb9264d3e84e1c0736
Shriram Ravi <shriramravi96@gmail.com>
2025-10-05 21:38:04 -0700

    Initial Commit

---
+++ breadcrumbsUITests/breadcrumbsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  breadcrumbsUITestsLaunchTests.swift
+//  breadcrumbsUITests
+//
+//  Created by Shriram R on 10/5/25.
+//
+
+import XCTest
+
+final class breadcrumbsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
